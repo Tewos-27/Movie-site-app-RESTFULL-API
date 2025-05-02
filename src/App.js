079c94f9c5dca1020fbc5fc11d7d5f21beab447c
Tewos-21 <tewodrosshimels54@gmail.com>
2025-05-02 04:28:29 -0700

    update all changes

--- src/App.js
+++ src/App.js
@@ -18,18 +18,14 @@ const App = () => {
         searchMovies('Spiderman');
   }, []); 
   return (
-    
     <div className="app">
-
        <h1>Tewos Movies</h1>
-
        <div className="search">
             <input 
                   placeholder="Search For Movies"
                   value={searchTerm}
                   onChange={(e) => setSearchTerm(e.target.value)}  
             />
-
             <img 
                   src={SearchIcon}
                   alt="Search"
